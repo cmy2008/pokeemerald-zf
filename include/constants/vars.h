b169c05aeaa48a02d297d583a31991c70f77ef51
cmy2008 <486150858@qq.com>
2024-01-31 17:11:31 +0800

    Map Add frist test

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -303,7 +303,7 @@
 #define VAR_SCOTT_BF_CALL_STEP_COUNTER       0x40F5
 #define VAR_RIVAL_RAYQUAZA_CALL_STEP_COUNTER 0x40F6
 #define VAR_BACK_ROOM                        0x40F7 // 后室
-#define VAR_UNUSED_0x40F8                    0x40F8 // Unused Var
+#define VAR_PVPDALAO                         0x40F8 // PVP大佬
 #define VAR_UNUSED_0x40F9                    0x40F9 // Unused Var
 #define VAR_UNUSED_0x40FA                    0x40FA // Unused Var
 #define VAR_UNUSED_0x40FB                    0x40FB // Unused Var
