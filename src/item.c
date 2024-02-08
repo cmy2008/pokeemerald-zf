c3f2b5aacfe3b141c5813237ae76e664563f8c0a
cmy2008 <486150858@qq.com>
2024-02-09 00:01:26 +0800

    T.2

--- src/item.c
+++ src/item.c
@@ -115,7 +115,7 @@ void GetBerryCountString(u8 *dst, const u8 *berryName, u32 quantity)
 
     txtPtr = StringCopy(dst, berryName);
     // *txtPtr = CHAR_SPACE;
-    StringCopy(txtPtr + 1, berryString);
+    StringCopy(txtPtr, berryString);
 }
 
 bool8 IsBagPocketNonEmpty(u8 pocket)
