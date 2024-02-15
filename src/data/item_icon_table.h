09b855f3e51e13dc0425e9a808ea69a0625f7824
cmy2008 <486150858@qq.com>
2024-02-15 22:03:48 +0800

    FIX BUG2

--- src/data/item_icon_table.h
+++ src/data/item_icon_table.h
@@ -859,7 +859,7 @@ const u32 *const gItemIconTable[ITEMS_COUNT + 1][2] =
     [ITEM_SWIFT_MOCHI] =  {gItemIcon_Mochi, gItemIconPalette_SwiftMochi},
     [ITEM_FRESH_START_MOCHI] =  {gItemIcon_Mochi, gItemIconPalette_FreshStartMochi},
     [ITEM_GLIMMERING_CHARM] = {gItemIcon_GlimmeringCharm, gItemIconPalette_GlimmeringCharm},
-    [ITEM_DIAMOND_PICKAXE] = {gMonBackPic_DiamondPickaxe, gMonPalette_DiamondPickaxe},
+    [ITEM_DIAMOND_PICKAXE] = {gItemIcon_ReturnToFieldArrow, gItemIconPalette_ReturnToFieldArrow},
     // Return to field arrow
     [ITEMS_COUNT] = {gItemIcon_ReturnToFieldArrow, gItemIconPalette_ReturnToFieldArrow},
 };
